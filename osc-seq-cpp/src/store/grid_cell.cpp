fc2669f2004b1d48b850f5db0c735744b6e44ee9
Acaruso <anthonycaruso102@gmail.com>
2020-11-18 07:24:19 -0500

    start refactor

--- osc-seq-cpp/src/store/grid_cell.cpp
+++ osc-seq-cpp/src/store/grid_cell.cpp
@@ -1,9 +1,27 @@
 #include "grid_cell.hpp"
 
+Target::Target() {}
+
+Target::Target(int row, int col) : row(row), col(col) {}
+
+Grid_Cell_Data::Grid_Cell_Data() {}
+
+Grid_Cell_Data::Grid_Cell_Data(
+    std::string key,
+    int value,
+    int min,
+    int max,
+    unsigned int options
+) : key(key), value(value), min(min), max(max), options(options) {}
+
 Grid_Cell::Grid_Cell()
-    : toggled(false), probability(100), retrigger(1), has_meta(false)
+: toggled(false)
 {
-    data.push_back({ "note", 48, 0, 100 });
-    meta_target.row = -1;
-    meta_target.col = -1;
+    data.push_back({ "probability", 100, 0, 101, Op_Int });
+    data.push_back({ "retrigger", 1, 1, 17, Op_Int });
+    data.push_back({ "note", 48, 0, 101, Op_Int | Op_Osc_Data });
+
+    // data.push_back({ "note", 48, 0, 100 });
+    // meta_target.row = -1;
+    // meta_target.col = -1;
 }
--- osc-seq-cpp/src/store/grid_cell.hpp
+++ osc-seq-cpp/src/store/grid_cell.hpp
@@ -3,33 +3,47 @@
 #include <string>
 #include <vector>
 
-struct Grid_Cell_Data
-{
-    std::string key;
-    int value;
-    int min;
-    int max;
+enum Grid_Cell_Data_Options {
+    Op_Int      = 0x01,
+    Op_Target   = 0x02,
+    Op_Osc_Data = 0x04,
 };
 
 struct Target
 {
     int row;
     int col;
+    Target();
+    Target(int row, int col);
+};
+
+struct Grid_Cell_Data
+{
+    std::string key;
+    int value;
+    int min;
+    int max;
+    Target target;
+    unsigned int options;
+    Grid_Cell_Data();
+    Grid_Cell_Data(std::string key, int value, int min, int max, unsigned int options);
 };
 
 struct Grid_Cell
 {
     bool toggled;
 
-    int probability;
-    int retrigger;
-    const static int num_fields = 2;
+    // int probability;
+    // int retrigger;
+    // const static int num_fields = 2;
 
     std::vector<Grid_Cell_Data> data;
 
-    bool has_meta;
-    Target meta_target;
-    const static int num_meta_fields = 1;
+    // bool has_meta;
+    // Target meta_target;
+    // const static int num_meta_fields = 1;
+
+    std::vector<Grid_Cell_Data> meta_data;
 
     Grid_Cell();
 };
