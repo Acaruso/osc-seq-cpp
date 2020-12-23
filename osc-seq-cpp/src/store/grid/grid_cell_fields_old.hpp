846659af2fc7be43d625b51751cf2ce308f7e0cc
Acaruso <anthonycaruso102@gmail.com>
2020-12-23 10:03:27 -0500

    start refactor

---
+++ osc-seq-cpp/src/store/grid/grid_cell_fields_old.hpp
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "../event_editor.hpp"
+#include "../register.hpp"
+
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+
+struct Int_Field
+{
+    int data;
+    int min;
+    int max;
+    int meta_mod;
+    int num_subfields = 1;
+    std::vector<bool> has_dropdown{false};
+
+    void update(Event_Editor& event_editor, int delta);
+    void reset_meta_mods();
+    std::string to_string();
+};
+
+struct Int_Pair_Field
+{
+    Int_Field first;
+    Int_Field second;
+    int num_subfields = 2;
+    std::vector<bool> has_dropdown{false, false};
+
+    void update(Event_Editor& event_editor, int delta);
+    void reset_meta_mods();
+    std::string to_string();
+};
+
+enum Source_Type
+{
+    Const,
+    RNG,
+    Reg0,
+    Reg1,
+    Num_Source_Type
+};
+
+enum Comp_Type
+{
+    LT,     // <
+    LT_Eq,  // <=
+    GT,     // >
+    GT_Eq,  // >=
+    Eq,     // ==
+    Num_Comp_Type
+};
+
+struct Conditional_Field
+{
+    Source_Type source1_type;
+    Int_Field source1_const;
+    Comp_Type comp_type;
+    Source_Type source2_type;
+    Int_Field source2_const;
+
+    int num_subfields = 5;
+    std::vector<bool> has_dropdown{true, false, true, true, false};
+
+    void update(Event_Editor& event_editor, int delta);
+    void reset_meta_mods();
+    std::vector<std::string> get_dropdown_list(Event_Editor& event_editor);
+    std::string to_string();
+};
+
+enum Mod_Dest
+{
+    Cond1_Const1,
+    Cond1_Const2,
+    Cond2_Const1,
+    Cond2_Const2,
+    Retrigger,
+    Note,
+    Duration,
+    Volume,
+    Pan,
+    Aux,
+    Delay1,
+    Delay2,
+    Mod_Reg0,
+    Mod_Reg1,
+    Num_Mod_Dest
+};
+
+enum Mod_Op
+{
+    Plus_Eq,
+    Minus_Eq,
+    Assn,
+    Num_Mod_Op
+};
+
+struct Mod_Field
+{
+    Int_Pair_Field target;
+    Mod_Dest mod_dest;
+    Mod_Op mod_op;
+    Source_Type source1_type;
+    Int_Field source1_const;
+
+    int num_subfields = 4;
+    std::vector<bool> has_dropdown{true, true, true, false};
+
+    void update(Event_Editor& event_editor, int delta);
+    void reset_meta_mods();
+    std::string to_string();
+};
+
+struct Value_Display_Res
+{
+    std::string text;
+    std::vector<std::pair<int, int>> subfield_idxs;
+};
+
+struct Event_Field
+{
+    std::string key;
+    bool is_osc_data;
+    std::variant<Int_Field, Int_Pair_Field, Conditional_Field, Mod_Field> value;
+
+    std::string get_value_str();
+    Value_Display_Res get_value_display();
+    int get_num_subfields();
+    std::vector<bool>& get_has_dropdown();
+    std::vector<std::string> get_dropdown_list(Event_Editor& event_editor);
+    void update(Event_Editor& event_editor, int delta);
+};
+
+struct Tab
+{
+    std::string key;
+    std::vector<Event_Field> fields;
+};
+
+std::string source_type_to_string(Source_Type type);
+std::string const_to_string(Source_Type type, Int_Field field);
+std::string comp_type_to_string(Comp_Type type);
+std::string mod_dest_to_string(Mod_Dest mod_dest);
+std::string mod_op_to_string(Mod_Op mod_op);
--- osc-seq-cpp/src/store/grid/grid_cell_old.cpp
+++ osc-seq-cpp/src/store/grid/grid_cell_old.cpp
@@ -12,73 +12,33 @@ Grid_Cell::Grid_Cell(int channel) : Grid_Cell()
 Grid_Cell::Grid_Cell()
 : toggled(false)
 {
-    tabs.push_back(
-        Tab{
-            "conds",
-            std::vector<Event_Field>{
-                make_conditional_field("cond1"),
-                make_conditional_field("cond2")
+    tabs.push_back({
+        "conds",
+        std::vector<Event_Field>{
+            {
+                "cond1",
+                false,
+                Conditional_Field{
+                    RNG,
+                    Int_Field{100, 0, 101, 0},
+                    LT_Eq,
+                    Const,
+                    Int_Field{100, 0, 101, 0}
+                }
+            },
+            {
+                "cond2",
+                false,
+                Conditional_Field{
+                    RNG,
+                    Int_Field{100, 0, 101, 0},
+                    LT_Eq,
+                    Const,
+                    Int_Field{100, 0, 101, 0}
+                }
             }
         }
-    );
-
-    tabs.push_back(
-        Tab{
-            "other",
-            std::vector<Event_Field>{
-                Event_Field{
-                    "retrigger",
-                    false,
-                    std::vector<Subfield>{
-                        Int_Subfield{"retrigger subfield", 1, 1, 17, 0}
-                    }
-                },
-                Event_Field{
-                    "note",
-                    true,
-                    std::vector<Subfield>{
-                        Int_Subfield{"note subfield", 48, 0, 101, 0}
-                    }
-                },
-                Event_Field{
-                    "duration",
-                    true,
-                    std::vector<Subfield>{
-                        Int_Subfield{"duration subfield", 100, 0, 1000, 0}
-                    }
-                },
-                Event_Field{
-                    "volume",
-                    true,
-                    std::vector<Subfield>{
-                        Int_Subfield{"volume subfield", 100, 0, 101, 0}
-                    }
-                },
-                Event_Field{
-                    "pan",
-                    true,
-                    std::vector<Subfield>{
-                        Int_Subfield{"pan subfield", 50, 0, 101, 0}
-                    }
-                },
-                Event_Field{
-                    "aux",
-                    true,
-                    std::vector<Subfield>{
-                        Int_Subfield{"aux subfield", 50, 0, 101, 0}
-                    }
-                },
-                Event_Field{
-                    "delay",
-                    false,
-                    std::vector<Subfield>{
-                        Int_Subfield{"delay subfield 1", 0, 0, 17, 0},
-                        Int_Subfield{"delay subfield 2", 2, 2, 17, 0},
-                    }
-                },
-            }
-        }
-    );
+    });
 
     tabs.push_back({
         "other",
